1fc4964d50becca67c5bdea618161af1e07d2cc0
Richard <lee.rich.m@gmail.com>
2020-10-10 21:06:53 -0500

    testing Header

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,9 +3,15 @@ import csv
 
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
+# def print_total(budget_data)
+
+
 #open and read csv
 with open(budget_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+
+
+    # # reading the headers
+    # csv_header = next(csv_file)
+    # print(f"Header: {csv_header}")
