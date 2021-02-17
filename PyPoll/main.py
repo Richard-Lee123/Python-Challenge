97f982b69acea8b56b15aaabda4773339388af13
Richard <lee.rich.m@gmail.com>
2021-02-16 20:03:28 -0600

    total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,4 +10,16 @@ with open (election_csv) as csvfile:
     csv_header = next(csvreader)
     # print(csv_header)
 
+    num_rows = 0
+    total = 0
+
+    data = list(csvreader)
+    total_votes = [item[0] for item in data]
+    # print(total_votes)
     
+    for row in data:
+        num_rows += 1
+
+        total += int(row[0])
+
+    print(f"Total Votes: {num_rows}")
