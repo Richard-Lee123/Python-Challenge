c1065fd6390bb60ce94e245e1d457da024fddb70
Richard <lee.rich.m@gmail.com>
2021-02-19 19:43:03 -0600

    just need to print out winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,28 +3,51 @@ import csv
 
 election_csv = os.path.join('Resources', 'election_data.csv')
 
+
+# def print_results(election_data):
+#     name = str(election_data[2])
+
+#     num_rows += 1 
+#     if row[2] not in dictt:
+#         dictt[row[2]] = 0
+#     dictt[row[2]] = dictt[row[2]]+1
+    
+#     candidates = row
+#     print({row}for row in dictt)
+
+
 with open (election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     # print(csvreader)
 
     csv_header = next(csvreader)
     # print(csv_header)
-
     num_rows = 0
     dictt = {}
 
+    # for row in csvreader:
+    #     print_results(row)
     data = list(csvreader)
     
+
+
     for row in data:
 
         num_rows += 1
         if row[2] not in dictt:
             dictt[row[2]]=0
         dictt[row[2]] = dictt[row[2]]+1
-    finalfinal = [{'Candidate':row,'Percentage of votes': f"{round((dictt[row]/num_rows)*100,3)}%", 'Vote Count': dictt[row]}for row in dictt]
+        
+    vote_stats = [{'Candidate':row,
+                    'Percentage of votes': f"{round((dictt[row]/num_rows)*100,3)}%",
+                    'Vote Count': dictt[row]}for row in dictt]
+
     print(f"Total Votes: {num_rows}")
-    print(finalfinal)
-    
+    print(vote_stats[0])
+    print(vote_stats[1])
+    print(vote_stats[2])
+    print(vote_stats[3])
+
     ##breaking down the steps    
     # final_list = [{'Candidate': row, 'Votes': dictt[row]} for row in dictt]
     # finalname = [{'Candidate':row}for row in dictt]
