94cd97f97595c382b9cc35f51722f66c022928a6
Richard <lee.rich.m@gmail.com>
2021-02-19 22:15:42 -0600

    finished just need to work on readme

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,17 +4,6 @@ import csv
 election_csv = os.path.join('Resources', 'election_data.csv')
 
 
-# def print_results(election_data):
-#     name = str(election_data[2])
-
-#     num_rows += 1 
-#     if row[2] not in dictt:
-#         dictt[row[2]] = 0
-#     dictt[row[2]] = dictt[row[2]]+1
-    
-#     candidates = row
-#     print({row}for row in dictt)
-
 
 with open (election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
@@ -22,14 +11,11 @@ with open (election_csv) as csvfile:
 
     csv_header = next(csvreader)
     # print(csv_header)
+
     num_rows = 0
     dictt = {}
 
-    # for row in csvreader:
-    #     print_results(row)
     data = list(csvreader)
-    
-
 
     for row in data:
 
@@ -42,11 +28,35 @@ with open (election_csv) as csvfile:
                     'Percentage of votes': f"{round((dictt[row]/num_rows)*100,3)}%",
                     'Vote Count': dictt[row]}for row in dictt]
 
+    #Finding the winner 
+    a = vote_stats[0]['Percentage of votes']
+    b = vote_stats[1]['Percentage of votes']
+    c = vote_stats[2]['Percentage of votes']
+    d = vote_stats[3]['Percentage of votes']
+
+    if a>b and a>c and a>d:
+        winner = vote_stats[0]['Candidate']
+    elif b>a and b>c and b>d:
+        winner = vote_stats[1]['Candidate']
+    elif c>a and c>b and c>d:
+        winner = vote_stats[2]['Candidate']
+    else: 
+        winner = vote_stats[3]['Candidate']
+
+
+    # final results to print    
+
+    print("Election Results")
+    print("------------------------------------------------------------------------------------")
     print(f"Total Votes: {num_rows}")
+    print("------------------------------------------------------------------------------------")
     print(vote_stats[0])
     print(vote_stats[1])
     print(vote_stats[2])
     print(vote_stats[3])
+    print("------------------------------------------------------------------------------------")
+    print(f"Winner: {winner}")
+    print("------------------------------------------------------------------------------------")
 
     ##breaking down the steps    
     # final_list = [{'Candidate': row, 'Votes': dictt[row]} for row in dictt]
@@ -58,5 +68,18 @@ with open (election_csv) as csvfile:
     # print(finalcount)
     # print(percentage)
 
+f_results = os.path.join("Resources","Results.txt")
 
+with open(f_results,'w') as text_file:
 
+    print("Election Results", file = text_file)
+    print("------------------------------------------------------------------------------------", file = text_file)
+    print(f"Total Votes: {num_rows}", file = text_file)
+    print("------------------------------------------------------------------------------------", file = text_file)
+    print(vote_stats[0], file = text_file)
+    print(vote_stats[1], file = text_file)
+    print(vote_stats[2], file = text_file)
+    print(vote_stats[3], file = text_file)
+    print("------------------------------------------------------------------------------------", file = text_file)
+    print(f"Winner: {winner}", file = text_file)
+    print("------------------------------------------------------------------------------------", file = text_file)
